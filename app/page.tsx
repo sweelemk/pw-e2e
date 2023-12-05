cb13a183a4136dd68d395fa4c81379ecd580e730
andrei-kashkan <andrei.kashkan@alphapoint.com>
2023-12-05 12:57:41 +0100

    test case

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>
+        Here is Playwright test
+      </h1>
       <button data-test="click-me">Click me</button>
     </main>
   )
